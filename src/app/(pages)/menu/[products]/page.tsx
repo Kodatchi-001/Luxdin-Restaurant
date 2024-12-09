72b9cdfd29cc2d59169aed0842b024a947230be0
Ahmed Hariri <Ahmedhariri58@gmail.com>
2024-12-09 21:15:56 +0100

    fix: Correct type handling for dynamic route params and replace logic

--- src/app/(pages)/menu/[products]/page.tsx
+++ src/app/(pages)/menu/[products]/page.tsx
@@ -1,6 +1,10 @@
 import Hero from "@/components/menu-details/hero";
 
-export default async function Products({ params }: { params: { products: string } }) {
+type ParamsType = {
+    products: string
+}
+
+export default async function Products({ params }: { params: ParamsType }) {
     const Tittle = params.products.replace(/-/g, ' ').replace(/And/g, '&');
 
     return <Hero Props={{ CardTittle: Tittle }} />
