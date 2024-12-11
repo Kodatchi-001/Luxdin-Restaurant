6132c6c56641cf1eec6432b8b695a532201f7bdb
Ahmed Hariri <Ahmedhariri58@gmail.com>
2024-12-11 19:07:38 +0100

    Fix async handling of params in Products component

--- src/app/(pages)/menu/[products]/page.tsx
+++ src/app/(pages)/menu/[products]/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "@/components/menu-details/hero";
 
-export default async function Products({ params }: { params: { products: string } }) {
-    const { products } = await params
+export default function Products({ params }: { params: { products: string } }) {
+    const { products } = params
     const Tittle = products.replace(/-/g, ' ').replace(/And/g, '&');
 
     return <Hero Props={{ CardTittle: Tittle }} />
