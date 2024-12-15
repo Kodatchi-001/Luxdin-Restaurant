8d8b4ecd15996c3b23f60b5cc48b8f4ee82b5647
Ahmed Hariri <Ahmedhariri58@gmail.com>
2024-12-15 17:34:36 +0100

    Create Page Reservattion

---
+++ src/app/(pages)/reservation/page.tsx
@@ -0,0 +1,11 @@
+import QuestionsHome from "@/components/home/questions";
+import Hero from "@/components/reservation/hero/inde";
+import Reservation_Information from "@/components/reservation/reservation-info";
+
+export default function Reservation() {
+    return <>
+        <Hero />
+        <Reservation_Information/>
+        <QuestionsHome/>
+    </>
+}
---
+++ src/components/reservation/reservation-info/index.tsx
@@ -0,0 +1,222 @@
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+
+export default function Reservation_Information() {
+    return <>
+        <section className="w-full flex justify-center lg:px-6 xl:px-24 mt-10">
+            <div className="w-full 2xl:max-w-7xl h-full flex flex-col gap-8 px-10 py-16 lg:p-16 bg-[#173534] text-white">
+                <div className="w-full flex gap-4">
+                    <Box className="w-1/2">
+                        <TextField fullWidth type="text" className="w-full" size="medium" label="First Name" name='FirstName'
+                            sx={{
+                                // Bordure par défaut
+                                '& fieldset': {
+                                    borderColor: '#d1a374', // Couleur de la bordure par défaut
+                                },
+                                // Bordure en focus
+                                '& .MuiOutlinedInput-root.Mui-focused fieldset': {
+                                    borderColor: '#d1a374', // Bordure rouge en focus
+                                },
+                                // Couleur du label par défaut
+                                '& .MuiInputLabel-root': {
+                                    color: '#d1a374',
+                                },
+                                // Couleur du label en focus
+                                '& .MuiInputLabel-root.Mui-focused': {
+                                    color: '#d1a374',
+                                },
+                                // Texte (valeur entrée) en blanc
+                                '& .MuiInputBase-input': {
+                                    color: 'white', // Texte en blanc
+                                },
+                            }}
+                        />
+                    </Box>
+                    <Box className="w-1/2">
+                        <TextField fullWidth type="text" className="w-full" size="medium" label="Last Name"
+                            sx={{
+                                // Bordure par défaut
+                                '& fieldset': {
+                                    borderColor: '#d1a374', // Couleur de la bordure par défaut
+                                },
+                                // Bordure en focus
+                                '& .MuiOutlinedInput-root.Mui-focused fieldset': {
+                                    borderColor: '#d1a374', // Bordure rouge en focus
+                                },
+                                // Couleur du label par défaut
+                                '& .MuiInputLabel-root': {
+                                    color: '#d1a374',
+                                },
+                                // Couleur du label en focus
+                                '& .MuiInputLabel-root.Mui-focused': {
+                                    color: '#d1a374',
+                                },
+                                // Texte (valeur entrée) en blanc
+                                '& .MuiInputBase-input': {
+                                    color: 'white', // Texte en blanc
+                                },
+                            }} />
+                    </Box>
+                </div>
+                <div className="w-full flex gap-4">
+                    <Box className="w-1/2">
+                        <TextField fullWidth type="text" className="w-full" size="medium" label="Phone Number" name='FirstName'
+                            sx={{
+                                // Bordure par défaut
+                                '& fieldset': {
+                                    borderColor: '#d1a374', // Couleur de la bordure par défaut
+                                },
+                                // Bordure en focus
+                                '& .MuiOutlinedInput-root.Mui-focused fieldset': {
+                                    borderColor: '#d1a374', // Bordure rouge en focus
+                                },
+                                // Couleur du label par défaut
+                                '& .MuiInputLabel-root': {
+                                    color: '#d1a374',
+                                },
+                                // Couleur du label en focus
+                                '& .MuiInputLabel-root.Mui-focused': {
+                                    color: '#d1a374',
+                                },
+                                // Texte (valeur entrée) en blanc
+                                '& .MuiInputBase-input': {
+                                    color: 'white', // Texte en blanc
+                                },
+                            }} />
+                    </Box>
+                    <Box className="w-1/2">
+                        <TextField fullWidth type="email" className="w-full" size="medium" label="Email"
+                            sx={{
+                                // Bordure par défaut
+                                '& fieldset': {
+                                    borderColor: '#d1a374', // Couleur de la bordure par défaut
+                                },
+                                // Bordure en focus
+                                '& .MuiOutlinedInput-root.Mui-focused fieldset': {
+                                    borderColor: '#d1a374', // Bordure rouge en focus
+                                },
+                                // Couleur du label par défaut
+                                '& .MuiInputLabel-root': {
+                                    color: '#d1a374',
+                                },
+                                // Couleur du label en focus
+                                '& .MuiInputLabel-root.Mui-focused': {
+                                    color: '#d1a374',
+                                },
+                                // Texte (valeur entrée) en blanc
+                                '& .MuiInputBase-input': {
+                                    color: 'white', // Texte en blanc
+                                },
+                            }} />
+                    </Box>
+                </div>
+                <Box className="w-full">
+                    <TextField fullWidth type="number" className="w-full" size="medium" label="Number of Guests"
+                        sx={{
+                            // Bordure par défaut
+                            '& fieldset': {
+                                borderColor: '#d1a374', // Couleur de la bordure par défaut
+                            },
+                            // Bordure en focus
+                            '& .MuiOutlinedInput-root.Mui-focused fieldset': {
+                                borderColor: '#d1a374', // Bordure rouge en focus
+                            },
+                            // Couleur du label par défaut
+                            '& .MuiInputLabel-root': {
+                                color: '#d1a374',
+                            },
+                            // Couleur du label en focus
+                            '& .MuiInputLabel-root.Mui-focused': {
+                                color: '#d1a374',
+                            },
+                            // Texte (valeur entrée) en blanc
+                            '& .MuiInputBase-input': {
+                                color: 'white', // Texte en blanc
+                            },
+                        }} />
+                </Box>
+                <div className="w-full flex gap-4">
+                    <Box className="w-1/2">
+                        <TextField fullWidth type="date" className="w-full" size="medium" label="Date"
+                            sx={{
+                                // Bordure par défaut
+                                '& fieldset': {
+                                    borderColor: '#d1a374', // Couleur de la bordure par défaut
+                                },
+                                // Bordure en focus
+                                '& .MuiOutlinedInput-root.Mui-focused fieldset': {
+                                    borderColor: '#d1a374', // Bordure rouge en focus
+                                },
+                                // Couleur du label par défaut
+                                '& .MuiInputLabel-root': {
+                                    color: '#d1a374',
+                                },
+                                // Couleur du label en focus
+                                '& .MuiInputLabel-root.Mui-focused': {
+                                    color: '#d1a374',
+                                },
+                                // Texte (valeur entrée) en blanc
+                                '& .MuiInputBase-input': {
+                                    color: 'white', // Texte en blanc
+                                },
+                            }} />
+                    </Box>
+                    <Box className="w-1/2">
+                        <TextField fullWidth type="time" className="w-full" size="medium" label="Time"
+                            sx={{
+                                // Bordure par défaut
+                                '& fieldset': {
+                                    borderColor: '#d1a374', // Couleur de la bordure par défaut
+                                },
+                                // Bordure en focus
+                                '& .MuiOutlinedInput-root.Mui-focused fieldset': {
+                                    borderColor: '#d1a374', // Bordure rouge en focus
+                                },
+                                // Couleur du label par défaut
+                                '& .MuiInputLabel-root': {
+                                    color: '#d1a374',
+                                },
+                                // Couleur du label en focus
+                                '& .MuiInputLabel-root.Mui-focused': {
+                                    color: '#d1a374',
+                                },
+                                // Texte (valeur entrée) en blanc
+                                '& .MuiInputBase-input': {
+                                    color: 'white', // Texte en blanc
+                                },
+                            }} />
+                    </Box>
+                </div>
+                <Box className="w-full">
+                    <TextField fullWidth multiline rows={4} type="text" className="w-full" size="medium" label="Additional Info" name='Email'
+                        sx={{
+                            // Bordure par défaut
+                            '& fieldset': {
+                                borderColor: '#d1a374', // Couleur de la bordure par défaut
+                            },
+                            // Bordure en focus
+                            '& .MuiOutlinedInput-root.Mui-focused fieldset': {
+                                borderColor: '#d1a374', // Bordure rouge en focus
+                            },
+                            // Couleur du label par défaut
+                            '& .MuiInputLabel-root': {
+                                color: '#d1a374',
+                            },
+                            // Couleur du label en focus
+                            '& .MuiInputLabel-root.Mui-focused': {
+                                color: '#d1a374',
+                            },
+                            // Texte (valeur entrée) en blanc
+                            '& .MuiInputBase-input': {
+                                color: 'white', // Texte en blanc
+                            },
+                        }} />
+                </Box>
+                <button className='w-full py-4 bg-[#d1a374] border border-[#d1a374] text-white text-lg rounded-md hover:bg-transparent hover:text-[#d1a374] duration-500'>
+                    Submit
+                </button>
+            </div>
+        </section>
+    </>
+}
